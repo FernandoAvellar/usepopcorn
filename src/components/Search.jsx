893b3c2a30c48faebd9d06ed502052a2889d70ad
Fernando Avellar Jr. <favellar@yahoo.com.br>
2024-09-16 11:08:05 -0300

    Add autofocus and Enter button to query

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,28 @@
+import { useEffect, useRef } from 'react';
+
 /* eslint-disable react/prop-types */
 export default function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(
+    function () {
+      function callBack(e) {
+        if (document.activeElement === inputEl.current) return;
+        if (e.code === 'Enter' || e.code === 'NumpadEnter') {
+          inputEl.current.focus();
+          setQuery('');
+        }
+      }
+
+      document.addEventListener('keydown', callBack);
+
+      return function () {
+        document.removeEventListener('keydown', callBack);
+      };
+    },
+    [setQuery]
+  );
+
   return (
     <input
       className="search"
@@ -7,6 +30,8 @@ export default function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      autoFocus
+      ref={inputEl}
     />
   );
 }
